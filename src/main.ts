91ef41e21af49d721cea0bc53c4d9eb0527a3010
Arthur Rogers <a23ryndin@gmail.com@>
2024-07-10 17:14:40 +0300

    debug logs

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ require("dotenv").config();
 const { PORT, HOST, SWAGGER_PREFIX } = process.env;
 
 async function bootstrap() {
+  console.log("Environment Variables:");
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+  console.log("PORT:", process.env.PORT);
+  console.log("HOST:", process.env.HOST);
+  console.log("DB_PORT:", process.env.DB_PORT);
+  console.log("DB_NAME:", process.env.DB_NAME);
+  console.log("DB_USERNAME:", process.env.DB_USERNAME);
+  console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+  console.log("MEDIA_STORAGE_URL:", process.env.MEDIA_STORAGE_URL);
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
